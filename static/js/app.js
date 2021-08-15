7100cb1e9cd104a47a037184a367bf46407092fd
ashleigh <ashleighdevito@gmail.com>
2021-08-15 16:52:12 -0400

    listening function

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,32 @@
 // from data.js
 let tableData = data;
 
+let form = d3.select("#datetime");
+let button = d3.select("#filter-btn");
 let tbody = d3.select("tbody");
 
 // YOUR CODE HERE!
-let inputField = d3.select("#datetime");
+button.on("click", runEnter);
+form.on("submit", runEnter);
 
-function dateselect(ufosighting) {
-    return ufosighting.datetime == inputField;
-}
+function runEnter() {
 
-let sightinglist = tableData.filter(dateselect);
+    d3.event.preventDefault();
 
-sightingslist.forEach((encounter) => {
-    let row = tbody.append("tr");
-    Object.entries(encounter).forEach(([key, value]) => {
-        let cell = row.append("td");
-        cell.text(value);
+    let inputValue = form.property("value");
+
+    function dateselect(encounter) {
+        return encounter.datetime == inputValue;
+    }
+
+    let sightinglist = tableData.filter(dateselect);
+
+    sightingslist.forEach((encounter) => {
+        let row = tbody.append("tr");
+        Object.entries(encounter).forEach(([key, value]) => {
+            let cell = row.append("td");
+            cell.text(value);
+        });
     });
-});
+
+};
